8b6d734aba8969a93298cac7c03df053d04bf65e
Rifat Azad <rifat.44.azad.rifs@gmail.com>
2024-12-01 13:20:52 +0600

    mm: ksm: KSM_RUN_START at boot

--- mm/ksm.c
+++ mm/ksm.c
@@ -288,10 +288,11 @@ static int ksm_nr_node_ids = 1;
 #endif
 
 #define KSM_RUN_STOP	0
+#define KSM_RUN_START	1
 #define KSM_RUN_MERGE	1
 #define KSM_RUN_UNMERGE	2
 #define KSM_RUN_OFFLINE	4
-static unsigned long ksm_run = KSM_RUN_STOP;
+static unsigned long ksm_run = KSM_RUN_START;
 static void wait_while_offlining(void);
 
 static DECLARE_WAIT_QUEUE_HEAD(ksm_thread_wait);
